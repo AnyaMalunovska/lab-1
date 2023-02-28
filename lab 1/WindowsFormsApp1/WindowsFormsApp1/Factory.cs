33a710a799d683c9af88fb9c0e8d056d087f0200
AnyaMalunovska <126598234+AnyaMalunovska@users.noreply.github.com>
2023-02-28 18:23:22 +0200

    Add files via upload

---
+++ lab 1/WindowsFormsApp1/WindowsFormsApp1/Factory.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public interface ITruckerFactory
+    {
+        ITruck CreateTruck();
+        IDriver CreateDriver();
+    }
+    public class FordTruckerFactory : ITruckerFactory
+    {
+        public ITruck CreateTruck()
+        {
+            return new FordTruck();
+        }
+
+        public IDriver CreateDriver()
+        {
+            return new FordDriver();
+        }
+    }
+
+    public class ChevyTruckerFactory : ITruckerFactory
+    {
+        public ITruck CreateTruck()
+        {
+            return new ChevyTruck();
+        }
+
+        public IDriver CreateDriver()
+        {
+            return new ChevyDriver();
+        }
+    }
+
+    public class VolvoTruckerFactory : ITruckerFactory
+    {
+        public ITruck CreateTruck()
+        {
+            return new VolvoTruck();
+        }
+
+        public IDriver CreateDriver()
+        {
+            return new VolvoDriver();
+        }
+    }
+    public interface ITruck
+    {
+        string GetBrand();
+    }
+
+    public interface IDriver
+    {
+        string GetCompanyName();
+    }
+
+    public class FordTruck : ITruck
+    {
+        public string GetBrand()
+        {
+            return "Ford";
+        }
+    }
+
+    public class ChevyTruck : ITruck
+    {
+        public string GetBrand()
+        {
+            return "Chevy";
+        }
+    }
+
+    public class VolvoTruck : ITruck
+    {
+        public string GetBrand()
+        {
+            return "Volvo";
+        }
+    }
+
+    public class FordDriver : IDriver
+    {
+        public string GetCompanyName()
+        {
+            return "Ford Trucking Inc.";
+        }
+    }
+
+    public class ChevyDriver : IDriver
+    {
+        public string GetCompanyName()
+        {
+            return "Chevy Trucking Inc.";
+        }
+    }
+
+    public class VolvoDriver : IDriver
+    {
+        public string GetCompanyName()
+        {
+            return "Volvo Trucking Inc.";
+        }
+    }
+}
